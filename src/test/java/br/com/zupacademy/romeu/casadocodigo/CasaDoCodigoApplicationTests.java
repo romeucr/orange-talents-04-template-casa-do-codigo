caf24fa0045bb6ff203d50f81a30176f6cf9fc81
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-04-28 22:26:08 +0200

    Setup inicial do projeto

---
+++ src/test/java/br/com/zupacademy/romeu/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.romeu.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
