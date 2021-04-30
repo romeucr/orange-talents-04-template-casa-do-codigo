3421e4b525ec812c10e380cfd568332e46990dc7
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-04-30 15:10:00 +0200

    fix : 0-0-3-email-autor-unico
    - criação de validação personalizada
    	- criado o método init() que é executado quando o controller é acionado
    		- nele foi adicionada a validação que deve ser realizada
    	- criada a classe VerificaEmailDuplicadoValidator responsável pela lógica de validação
    		- método supports() define que tipo de objeto a validação suporta
    		- método validate() implementa a validação
    
    Este tipo de validação lança uma MethodArgumentNotFoundException que já está sendo capturada e tratada pelo RestControllerAdvice

--- src/main/java/br/com/zupacademy/romeu/casadocodigo/autor/AutorController.java
+++ src/main/java/br/com/zupacademy/romeu/casadocodigo/autor/AutorController.java
@@ -1,11 +1,10 @@
 package br.com.zupacademy.romeu.casadocodigo.autor;
 
+import br.com.zupacademy.romeu.casadocodigo.compartilhado.VerificaEmailDuplicadoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -18,14 +17,20 @@ public class AutorController {
   @Autowired
   AutorRepository autorRepository;
 
+  @Autowired
+  private VerificaEmailDuplicadoValidator verificaEmailDuplicadoValidator;
+
+  @InitBinder
+  public void init(WebDataBinder binder) {
+    // método executado sempre que o controlador (algum dos métodos) é chamado
+    binder.addValidators(verificaEmailDuplicadoValidator);
+  }
+
   @PostMapping
   @Transactional
   public ResponseEntity<?> cadastraAutor(@RequestBody @Valid NovoAutorRequest novoAutorRequest) {
 
     Optional<Autor> optAutor = autorRepository.findByEmail(novoAutorRequest.getEmail());
-    if (optAutor.isPresent()) {
-      return ResponseEntity.badRequest().body("E-mail já cadastrado");
-    }
 
     Autor autor = novoAutorRequest.toModel();
     autorRepository.save(autor);
---
+++ src/main/java/br/com/zupacademy/romeu/casadocodigo/compartilhado/VerificaEmailDuplicadoValidator.java
@@ -0,0 +1,44 @@
+package br.com.zupacademy.romeu.casadocodigo.compartilhado;
+
+
+import br.com.zupacademy.romeu.casadocodigo.autor.Autor;
+import br.com.zupacademy.romeu.casadocodigo.autor.AutorRepository;
+import br.com.zupacademy.romeu.casadocodigo.autor.NovoAutorRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+
+@Component
+public class VerificaEmailDuplicadoValidator implements Validator {
+
+  @Autowired
+  private AutorRepository autorRepository;
+
+  @Override
+  public boolean supports(Class<?> aClass) {
+    return NovoAutorRequest.class.isAssignableFrom(aClass);
+    // qual é o tipo de parâmetro onde a verificação será realizada.
+    // verifica se a classe que está chegando pelo parâmetro é filha o a própria NovoAutorRequest
+  }
+
+  @Override
+  public void validate(Object o, Errors errors) {
+    // verifica se já houve algum erro anterior identificado pelos validadores nativos do spring.
+    // se já há erros, não executa a validação personalizada.
+    if (errors.hasErrors()) {
+      return;
+    }
+
+    NovoAutorRequest request = (NovoAutorRequest) o;
+    Optional<Autor> possivelAutor = autorRepository.findByEmail(request.getEmail());
+
+    if (possivelAutor.isPresent()) {
+      // campo, código de erro, mensagem de erro
+      errors.rejectValue("email", "EmailCadastrado", "{EmailCadastrado.email}");
+    }
+
+  }
+}
